964bf6a194c0699726b574b74bc15d744821fb37
sowmya dinavahi and tyler stephenson <tyler@>
2017-06-27 08:50:57 -0700

    Add boiler plate files and create a post model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+    flash[:notice] = "Post successfully added!"
+      redirect_to  posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post= Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "Post successfully updated!"
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      flash[:notice] = "Post successfully removed!"
+      redirect_to posts_path
+    end
+  end
+
+private
+  def post_params
+    params.require(:post).permit(:title, :url)
+  end
+end
---
+++ db/migrate/20170627154159_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :posts do |t|
+      t.column :title, :string
+      t.column :url, :string
+
+      t.timestamps
+    end
+  end
+end
